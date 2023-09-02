47ef2dfe3090d4c8730fa180a711ce740a615176
KAnderson <kylaa2@uw.edu>
2023-09-01 18:38:55 -0700

    placed variables and operators inside my calculator.js

---
+++ lesson02/calculator.js
@@ -0,0 +1,13 @@
+let a = 'sunflower'; 
+let b = 'basil'; 
+let c = 40
+let d = '40'
+
+if ( 15 / 5 === 5) { 
+  console.log(a); 
+} else if (a === 'butterfly') { 
+  console.log(b); 
+} else if (c == '40') {
+  console.log(2 + d);
+}
+
