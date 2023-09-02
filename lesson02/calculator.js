f48ee0df57bda82c978d542a6d3136ec0b08b0c7
KAnderson <kylaa2@uw.edu>
2023-09-02 08:20:57 -0700

    calculator.js created but still figuring out how to push information to remote repository

--- lesson02/calculator.js
+++ lesson02/calculator.js
@@ -1,13 +1,12 @@
 let a = 'sunflower'; 
 let b = 'basil'; 
 let c = 40
-let d = '40'
+let d = 30
 
 if ( 15 / 5 === 5) { 
   console.log(a); 
 } else if (a === 'butterfly') { 
   console.log(b); 
-} else if (c == '40') {
+} else if (c > 33) {
   console.log(2 + d);
 }
-
