3dd114a4ca7c1996536c6b6ff926a72c8d0a26ee
KAnderson <kylaa2@uw.edu>
2023-10-30 13:58:17 -0700

    Created a button inside my html. Created a onclick event handler for my button. Created my Atm function. Created a while loop inside my Atm function. Created if else statments inside my while loop. The if statement has to allow my website user to input Q to quit the program

---
+++ lesson05/exercise-five.js
@@ -0,0 +1,22 @@
+function Atm(){
+    let balance = 0;
+    while (true){
+    let input = prompt ( 'Please enter Q to quit. Please enter W to withdraw. Please enter D to deposit. Please enter B to view balance');
+    if (input === "Q"){
+        break;
+    }
+
+}  
+alert(result);
+}
+
+
+
+
+
+
+
+
+
+
+
