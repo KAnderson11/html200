98533497485fbf0aacf98964f51cacc7b977638b
KAnderson <kylaa2@uw.edu>
2023-10-31 10:47:23 -0700

    Created javascript more javascript prompt coding. Used Number () to ensure the machine can have the inputs in the format needed. Created more coding for the alert messages. Used javascript assignment operators -= to determine the balance minus the amount withdrawn. Used javascript assignment operators += to determine the balance plus the amount deposited.

--- lesson05/exercise-five.js
+++ lesson05/exercise-five.js
@@ -1,22 +1,30 @@
-function Atm(){
-    let balance = 0;
-    while (true){
-    let input = prompt ( 'Please enter Q to quit. Please enter W to withdraw. Please enter D to deposit. Please enter B to view balance');
-    if (input === "Q"){
-        break;
+    function Atm() {
+        let balance = 0;
+        while (true) {
+          let input = prompt('Please enter Q to quit. Please enter W to withdraw. Please enter D to deposit. Please enter B to view balance.');
+          if (input === "Q") {
+            break;
+          } else if (input === "W") {
+            let amount = Number(prompt('Please enter the amount you want to withdraw'));
+              if (balance < amount) {
+                alert('You have insufficient funds!');
+            } else {
+              balance -= amount;
+                alert('You have withdrawn $' + (amount)); 
+                alert('Your new balance is $'+ (balance)); 
+            }
+          } else if (input === "D") {
+            let amount = Number(prompt('Please enter the amount you are depositing'));
+            balance += amount;
+              alert('You have deposited $' + (amount));
+              alert('Your new balance is $' + (balance));
+          } else if (input === "B") {
+              alert('Your current account balance is $' + (balance));
+          } else {
+              alert('The information is not recognized by our banking system! Please enter Q to quit. W to withdraw. D to deposit. B to view balance.' );
+          }
+        }
+
     }
-
-}  
-alert(result);
-}
-
-
-
-
-
-
-
-
-
 
 
